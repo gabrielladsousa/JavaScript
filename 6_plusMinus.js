7b7bb69d9ea78cfc4add13de19db566e9a82f6f7
Gabriella Sousa <95770849+gabrielladsousa@users.noreply.github.com>
2024-08-20 23:34:29 -0300

    Plus Minus
     Calcula a proporção dos elementos de uma matriz que são positivos , negativos e zero . Imprime o valor decimal de cada fração em uma nova linha com 6 casas após a vírgula.

---
+++ 6_plusMinus.js
@@ -0,0 +1,59 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    let positivo = 0;
+    let negativo = 0;
+    let zero = 0;
+    
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 0){
+            zero += 1;
+        }
+        else{
+            if(arr[i] > 0){
+                positivo += 1;
+            }
+            else{
+                negativo += 1;
+            }
+        }
+    }
+    
+    console.log((positivo / arr.length) || 0);
+    console.log((negativo / arr.length) || 0);
+    console.log((zero / arr.length) || 0);
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
